143d8d24affd0d3c798d5b691b16baed29e97c2b
rayane hachem <rayanehchm@gmail.com>
2021-11-12 15:00:36 +0100

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/new'
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/new'
+  # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # GET "lists" -- index list
+  # GET "lists/42" -- show list
+  # GET "lists/new" -- new list
+  # POST "lists" -- create list
+  root to: 'lists#index'
+  resources :lists, only: [:show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
